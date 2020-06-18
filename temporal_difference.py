4f6a12b68716148492e38a1acb0be77ba8f21661
Junyoungpark <Junyoungpark@kaist.ac.kr>
2020-06-18 21:42:31 +0900

    td prediction done

--- temporal_difference.py
+++ temporal_difference.py
@@ -8,13 +8,14 @@ class TDAgent:
                  num_states: int,
                  num_actions: int,
                  epsilon: float,
-                 lr: float):
+                 lr: float,
+                 n_step: int):
         self.gamma = gamma
         self.num_states = num_states
         self.num_actions = num_actions
         self.lr = lr
         self.epsilon = epsilon
-        # self.n_step = n_step
+        self.n_step = n_step
 
         # Initialize state value function V and action value function Q
         self.v = None
@@ -31,7 +32,7 @@ class TDAgent:
         self.q = np.zeros(shape=(self.num_states, self.num_actions))
 
     def reset_policy(self):
-        self.q = np.zeros(shape=(self.num_states, self.num_actions))
+        self._policy_q = np.zeros(shape=(self.num_states, self.num_actions))
 
     def get_action(self, state):
         prob = np.random.uniform(0.0, 1.0, 1)
@@ -39,21 +40,32 @@ class TDAgent:
         if prob <= self.epsilon:  # random
             action = np.random.choice(range(self.num_actions))
         else:  # greedy
-            action = self.q[state, :].argmax()
+            action = self._policy_q[state, :].argmax()
         return action
 
     def update(self, episode):
-        # perform 1-step TD (a.k.a TD(0))
-
         states, actions, rewards = episode
-        next_states = states[1:] + [-1]  # -1 is a place holder
-        dones = np.zeros_like(states)
-        dones[-1] = 1
+        ep_len = len(states)
 
-        for s, a, r, ns, done in zip(states, actions, rewards, next_states, dones):
-            # TD target
-            td_target = r + self.gamma * self.v[ns] * (1 - done)
-            self.v[s] += self.lr * (td_target - self.v[s])
+        states += [0] * (self.n_step + 1)  # append dummy states
+        rewards += [0] * (self.n_step + 1)  # append dummy rewards
+        dones = [0] * ep_len + [1] * (self.n_step + 1)
+
+        kernel = np.array([self.gamma ** i for i in range(self.n_step)])
+        for i in range(ep_len):
+            s = states[i]
+            ns = states[i + self.n_step]
+            done = dones[i]
+
+            # compute n-step TD target
+            g = np.sum(rewards[i:i + self.n_step] * kernel)
+            g += (self.gamma ** self.n_step) * self.v[ns] * (1 - done)
+            self.v[s] += self.lr * (g - self.v[s])
+
+    def sample_update(self, state, action, reward, next_state, done):
+        # 1-step TD target
+        td_target = reward + self.gamma * self.v[next_state] * (1 - done)
+        self.v[state] += self.lr * (td_target - self.v[state])
 
     def decaying_epsilon(self, factor):
         self.epsilon *= factor
@@ -127,3 +139,42 @@ class QLeaner(TDAgent):
         s, a, r, ns = state, action, reward, next_state
         td_target = r + self.gamma * self.q[ns, :].max() * (1 - done)
         self.q[s, a] += self.lr * (td_target - self.q[s, a])
+
+
+if __name__ == '__main__':
+    from envs.gridworld import GridworldEnv
+
+    nx, ny = 4, 4
+    env = GridworldEnv([ny, nx])
+
+    n_step_td_agent = TDAgent(gamma=1.0,
+                              num_states=nx * ny,
+                              num_actions=4,
+                              epsilon=1.0,
+                              lr=1e-2,
+                              n_step=1)
+
+
+    def run_episode(env, agent):
+        env.reset()
+        states = []
+        actions = []
+        rewards = []
+
+        while True:
+            state = env.observe()
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(action)
+
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+
+            if done:
+                break
+
+        episode = (states, actions, rewards)
+        agent.update(episode)
+
+
+    run_episode(env, n_step_td_agent)
